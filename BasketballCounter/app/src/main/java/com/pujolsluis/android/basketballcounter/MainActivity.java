086f44fb801840e06db0496603f71be2aabe08e2
Luis Pujols <luispujols@hotmail.com>
2016-12-07 00:25:09 -0400

    ADD "Created the MainActivity methods to increase points in the counters of each team, and also created the reset score button with its respective method now the application is completly funtional. Also added a toast after you press the reset score button to make it more user friendly"

--- BasketballCounter/app/src/main/java/com/pujolsluis/android/basketballcounter/MainActivity.java
+++ BasketballCounter/app/src/main/java/com/pujolsluis/android/basketballcounter/MainActivity.java
@@ -1,22 +1,80 @@
 package com.pujolsluis.android.basketballcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int teamAScore = 0;
-    int teamBScore = 0;
+    Integer teamAScore = 0;
+    Integer teamBScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView teamAScoreTextView = (TextView) findViewById(R.id.textView_score_teamA);
-        TextView teamBScoreTextView = (TextView) findViewById(R.id.textView_score_teamB);
-        teamAScoreTextView.setText("5");
     }
 
-    //void addOnePoint
+
+    public void addOnePoint(View v){
+        Button teamAFreePointButton = (Button) findViewById(R.id.button_teamA_freePoint);
+        TextView teamAScoreTextView = (TextView) findViewById(R.id.textView_score_teamA);
+        TextView teamBScoreTextView = (TextView) findViewById(R.id.textView_score_teamB);
+
+        Button buttonViewTeam = (Button) v;
+
+        if(buttonViewTeam.equals(teamAFreePointButton)){
+            teamAScore++;
+            teamAScoreTextView.setText(teamAScore.toString());
+        }else{
+            teamBScore++;
+            teamBScoreTextView.setText(teamBScore.toString());
+        }
+        return;
+    }
+
+    public void addTwoPoint(View v){
+        Button teamATwoPointButton = (Button) findViewById(R.id.button_teamA_twoPoints);
+        TextView teamAScoreTextView = (TextView) findViewById(R.id.textView_score_teamA);
+        TextView teamBScoreTextView = (TextView) findViewById(R.id.textView_score_teamB);
+
+        Button buttonViewTeam = (Button) v;
+
+        if(buttonViewTeam.equals(teamATwoPointButton)){
+            teamAScore += 2;
+            teamAScoreTextView.setText(teamAScore.toString());
+        }else{
+            teamBScore += 2;
+            teamBScoreTextView.setText(teamBScore.toString());
+        }
+    }
+
+    public void addThreePoint(View v){
+        Button teamAThreePointButton = (Button) findViewById(R.id.button_teamA_threePoints);
+        TextView teamAScoreTextView = (TextView) findViewById(R.id.textView_score_teamA);
+        TextView teamBScoreTextView = (TextView) findViewById(R.id.textView_score_teamB);
+
+        Button buttonViewTeam = (Button) v;
+
+        if(buttonViewTeam.equals(teamAThreePointButton)){
+            teamAScore += 3;
+            teamAScoreTextView.setText(teamAScore.toString());
+        }else{
+            teamBScore += 3;
+            teamBScoreTextView.setText(teamBScore.toString());
+        }
+    }
+
+    public void resetScore(View v){
+        teamAScore = teamBScore = 0;
+        TextView teamAScoreTextView = (TextView) findViewById(R.id.textView_score_teamA);
+        TextView teamBScoreTextView = (TextView) findViewById(R.id.textView_score_teamB);
+        teamAScoreTextView.setText("0");
+        teamBScoreTextView.setText("0");
+        Toast.makeText(this, "The score has been reset", Toast.LENGTH_SHORT).show();
+
+    }
 }
