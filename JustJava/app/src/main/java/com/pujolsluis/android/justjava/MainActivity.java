616ad8b470429a0d47d2da2d8e76bac1b6eb7ab2
Luis Pujols <luispujols@hotmail.com>
2016-12-07 18:23:39 -0400

    ADD "Created the methods for the buttons, and the order button. now the application is completly functional"

--- JustJava/app/src/main/java/com/pujolsluis/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/pujolsluis/android/justjava/MainActivity.java
@@ -1,13 +1,58 @@
 package com.pujolsluis.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Integer quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void increaseQuantity(View v){
+        quantity++;
+        TextView thankyouTextView = (TextView) findViewById(R.id.textView_thankYou);
+        TextView quantityTextView = (TextView) findViewById(R.id.textView_QuantityTotal);
+        TextView orderTotalTextView = (TextView) findViewById(R.id.textView_priceTotal);
+
+        if(thankyouTextView.getVisibility() == View.VISIBLE){
+            orderTotalTextView.setText("$0");
+            thankyouTextView.setVisibility(View.GONE);
+        }
+
+        quantityTextView.setText(quantity.toString());
+
+    }
+
+    public void decreaseQuantity(View v){
+        TextView thankyouTextView = (TextView) findViewById(R.id.textView_thankYou);
+        TextView orderTotalTextView = (TextView) findViewById(R.id.textView_priceTotal);
+        TextView quantityTextView = (TextView) findViewById(R.id.textView_QuantityTotal);
+
+        if(thankyouTextView.getVisibility() == View.VISIBLE){
+            orderTotalTextView.setText("$0");
+            thankyouTextView.setVisibility(View.GONE);
+        }
+
+        if(quantity > 0){
+            quantity--;
+            quantityTextView.setText(quantity.toString());
+        }
+    }
+
+    public void orderCoffe(View v){
+        TextView thankyouTextView = (TextView) findViewById(R.id.textView_thankYou);
+        TextView orderTotalTextView = (TextView) findViewById(R.id.textView_priceTotal);
+
+        Integer orderTotal = quantity * 5;
+        orderTotalTextView.setText("$" + orderTotal.toString());
+        thankyouTextView.setVisibility(View.VISIBLE);
+    }
+
 }
