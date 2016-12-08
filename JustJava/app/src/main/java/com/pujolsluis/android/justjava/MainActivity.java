b97396f1a7ffd2d89d61883845d4ce0f50572658
Luis Pujols <luispujols@hotmail.com>
2016-12-07 20:57:29 -0400

    ADD "Created the toppings layout and added the logic of toppings, also modified the order total depending on the topping selected"

--- JustJava/app/src/main/java/com/pujolsluis/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/pujolsluis/android/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.pujolsluis.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         thankyouTextView = (TextView) findViewById(R.id.textView_thankYou);
         quantityTextView = (TextView) findViewById(R.id.textView_QuantityTotal);
         orderTotalTextView = (TextView) findViewById(R.id.textView_priceTotal);
+
     }
 
 
@@ -49,9 +52,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void orderCoffe(View v){
-
+        EditText customerName = (EditText) findViewById(R.id.editText_personName);
         Integer orderTotal = quantity * 5;
+
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.checkBox_whippedCream);
+        CheckBox chocolate = (CheckBox) findViewById(R.id.checkBox_chocolate);
+
+        String name = customerName.getText().toString();
+
+        if(whippedCream.isChecked()) orderTotal++;
+        if(chocolate.isChecked()) orderTotal += 2;
+
         orderTotalTextView.setText("$" + orderTotal.toString());
+        thankyouTextView.setText("Thank you! " + name);
         thankyouTextView.setVisibility(View.VISIBLE);
     }
 
