77f029052007b318a152b5d7663d09287ee4a34c
Luis Pujols <luispujols@hotmail.com>
2016-12-12 13:59:51 -0400

    UPDATE "Refactored the layout, to include the reset nad estatus buttons, plus an image for the song, and the text views for the song title and song author. The layout is now completed"

--- MusicPlayer/app/src/main/java/com/pujolsluis/android/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/pujolsluis/android/musicplayer/MainActivity.java
@@ -1,13 +1,42 @@
 package com.pujolsluis.android.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer currSong = new MediaPlayer().create(this, R.raw.give_me_love);
+        final Button playButton = (Button) findViewById(R.id.button_Play);
+        final Button pauseButton = (Button) findViewById(R.id.button_Pause);
+
+        // Setting the onClickListener for the play button
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currSong.start();
+                Log.d("MainActivity", "Play Song");
+            }
+        });
+
+        // Setting onClickListener for the pause button
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currSong.isPlaying()) currSong.pause();
+                Log.d("MainActivity", "Pause Song");
+            }
+        });
     }
+
 }
