f1be2130a1a138af3277a95c1d6ec55a5f084ddb
Luis Pujols <luispujols@hotmail.com>
2016-12-18 00:02:16 -0400

    ADD "Created the NumbersFragment and now my NumbersActivity uses the NumbersFragment to display the list of numbers"

---
+++ Miwok Project/app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -0,0 +1,121 @@
+package com.example.android.miwok;
+
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NumbersFragment extends Fragment {
+        MediaPlayer rootMediaPlayer;
+        AudioManager audioManager;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.word_list, container, false);
+
+        audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        final ArrayList<Word> numbersArray = new ArrayList<Word>();
+
+        numbersArray.add(new Word("lutti", "one", R.drawable.number_one, R.raw.number_one));
+        numbersArray.add(new Word("otiiko", "two", R.drawable.number_two, R.raw.number_two));
+        numbersArray.add(new Word("tolookosu", "three", R.drawable.number_three, R.raw.number_three));
+        numbersArray.add(new Word("oyyisa", "four", R.drawable.number_four, R.raw.number_four));
+        numbersArray.add(new Word("massokka", "five", R.drawable.number_five, R.raw.number_five));
+        numbersArray.add(new Word("temmokka", "six", R.drawable.number_six, R.raw.number_six));
+        numbersArray.add(new Word("kenekaku", "seven", R.drawable.number_seven, R.raw.number_seven));
+        numbersArray.add(new Word("kawinta", "eight", R.drawable.number_eight, R.raw.number_eight));
+        numbersArray.add(new Word("wo’e", "nine", R.drawable.number_nine, R.raw.number_nine));
+        numbersArray.add(new Word("na’aacha", "ten", R.drawable.number_ten, R.raw.number_ten));
+
+
+        WordAdapter itemsAdapter = new WordAdapter(getActivity(), numbersArray, R.color.category_numbers);
+
+
+        final ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                releaseMediaPlayer();
+
+                int result = audioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    rootMediaPlayer = MediaPlayer.create(getActivity(), numbersArray.get(position).getAudioResourceId());
+                    rootMediaPlayer.start();
+                    rootMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            releaseMediaPlayer();
+                        }
+                    });
+                }
+            }
+        });
+        return rootView;
+    }
+
+
+    public void releaseMediaPlayer(){
+        if(rootMediaPlayer != null){
+            rootMediaPlayer.release();
+            rootMediaPlayer = null;
+            audioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
+
+
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                // both cases the same way because our app is playing short sound files.
+
+                // Pause playback and reset player to the start of the file. That way, we can
+                // play the word from the beginning when we resume playback.
+                rootMediaPlayer.pause();
+                rootMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                rootMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+}
