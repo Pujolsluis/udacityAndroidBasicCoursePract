b4f3d9bb4bfc89b7cb898afaad09a650921d1d0f
Luis Pujols <luispujols@hotmail.com>
2016-12-18 00:43:24 -0400

    ADD "Created the FragmentPagerAdapter and deleted all the unused activities, now my MainActivity has a ViewPager that displays all the word fragments and the app is completly functional"

--- Miwok Project/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok Project/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,11 +15,9 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,49 +28,10 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        TextView numbers = (TextView) findViewById(R.id.numbers);
-        // Setting numbers click listener
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent numbersIntent = new Intent(v.getContext(), NumbersActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        TextView colors = (TextView) findViewById(R.id.colors);
-        // Setting colors click listener
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent colorsIntent = new Intent(v.getContext(), ColorsActivity.class);
-                startActivity(colorsIntent);
-            }
-        });
+        SimpleFragmentPagerAdapter pagerAdapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
 
-        TextView family = (TextView) findViewById(R.id.family);
-        // Setting family click listener
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent familyIntent = new Intent(v.getContext(), FamilyActivity.class);
-                startActivity(familyIntent);
-            }
-        });
-
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-        // Setting phrases click listener
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent phrasesIntent = new Intent(v.getContext(), PhrasesActivity.class);
-                startActivity(phrasesIntent);
-            }
-        });
-    }
-
-    public void openNumbersList(View view){
-        Intent openIntent = new Intent(this, NumbersActivity.class);
-        startActivity(openIntent);
+        viewPager.setAdapter(pagerAdapter);
     }
 }
