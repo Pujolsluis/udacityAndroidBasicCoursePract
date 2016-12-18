23e971c0b6232d3499d019449a6bef0eb8831ddf
Luis Pujols <luispujols@hotmail.com>
2016-12-18 01:26:31 -0400

    ADD "Added the TabLayout to the ViewPager, and now the app display the tabs for each category"

--- Miwok Project/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok Project/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -30,8 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        SimpleFragmentPagerAdapter pagerAdapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        SimpleFragmentPagerAdapter pagerAdapter = new SimpleFragmentPagerAdapter(this, getSupportFragmentManager());
 
         viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sibling_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
