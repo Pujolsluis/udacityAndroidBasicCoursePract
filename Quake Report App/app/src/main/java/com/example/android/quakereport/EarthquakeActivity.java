9d44abdba61a2439798133213751273b92f25c89
Luis Pujols <luispujols@hotmail.com>
2017-01-08 17:56:10 -0400

    ADD 'Refactored the code to make the API call and get the earthquake data with the specifications i need'

--- Quake Report App/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ Quake Report App/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -18,6 +18,7 @@ package com.example.android.quakereport;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -30,16 +31,21 @@ public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
     public Context context = this;
+    ArrayList<Earthquake> earthquakes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-
-        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        FetchInBackgroundEarthquakeData fetchQuakeData = new FetchInBackgroundEarthquakeData();
+        fetchQuakeData.execute("http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10");
 //        Log.v(LOG_TAG, earthquakes.get(0).getmLocation());
 
+
+    }
+
+    private void updateUI(){
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
@@ -58,6 +64,24 @@ public class EarthquakeActivity extends AppCompatActivity {
                 startActivity(websiteIntent);
             }
         });
+    }
 
+    private class FetchInBackgroundEarthquakeData extends AsyncTask<String, Void, ArrayList<Earthquake> >{
+
+        @Override
+        protected ArrayList<Earthquake> doInBackground(String... urls) {
+
+            if(urls.length < 1 || urls[0] == null) return null;
+            ArrayList<Earthquake> earthquakeList = QueryUtils.fetchEarthquakeData(urls[0]);
+
+            return earthquakeList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Earthquake> earthquakesList) {
+            earthquakes = earthquakesList;
+            updateUI();
+            return;
+        }
     }
 }
